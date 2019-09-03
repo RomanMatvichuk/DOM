506518922493b575e939e6b12b57686d877edc75
Roman Matvichuk <maro19xr@student.ju.se>
2019-09-03 22:51:55 +0200

    Changed script

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ var newHTML = '<div id="secondBlock"><p id="newText"></p></div>';
 block1.insertAdjacentHTML('afterend', newHTML);
 
 var newText = document.getElementById("newText");
+newText.innerText = 'Jag är bra på:\n'
 
 for (i=0; i<knowHowArr.length; i++) {
-     
+
         newText.innerText += knowHowArr[i] + '\n'
 
 }
