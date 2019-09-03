c3d0d58bc2501f0cfd8cf52ca9c34f6c087cb4d0
Roman Matvichuk <maro19xr@student.ju.se>
2019-09-03 23:30:31 +0200

    Fixed script

--- script.js
+++ script.js
@@ -7,17 +7,10 @@ var newHTML = '<div id="secondBlock"><p id="newText"></p></div>';
 block1.insertAdjacentHTML('afterend', newHTML);
 
 var newText = document.getElementById("newText");
-newText.innerText = 'Jag är bra på: ';
+newText.innerText = 'Jag är bra på:';
 
 for (i=0; i<knowHowArr.length; i++) {
 
-   /*     if (i < knowHowArr.length - 1) {
-                newText.innerText += knowHowArr[i] + ', ';
-        }
-        else {
-                newText.innerText += knowHowArr[i];
-        }
-*/
         var newLine = document.createElement("p");
         var text = document.createTextNode(knowHowArr[i]);
         newLine.appendChild(text);
